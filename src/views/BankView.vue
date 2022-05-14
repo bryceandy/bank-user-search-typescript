4948f0a66aeac04a80b1f11c21d1cd943b02cba5
bryceandy <bryceandy@rocketmail.com>
2022-05-14 15:07:32 +0300

    List banks in a dropdown

--- src/views/BankView.vue
+++ src/views/BankView.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="px-5 sm:px-8 lg:px-20"></div>
+  <div class="px-5 sm:px-8 lg:px-20 lg:pb-12">
+    <h1 class="text-5xl font-bold">Bank Users</h1>
+    <div class="my-6 flex">
+      <label for="select-bank">Please select a bank:</label>
+      <select v-model="activeBank" name="select-bank" id="select-bank" class="px-4 py-3 rounded-full">
+        <option :value="id" v-for="{ id, name } in banks" :key="id">{{ name }}</option>
+      </select>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import sourceData from '@/data'
+import { reactive, ref } from 'vue'
+
+const banks = reactive(sourceData.banks)
+const activeBank = ref('')
 </script>
