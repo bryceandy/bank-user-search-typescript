46e29098efea9823b154fbc99ac94ecf63c39808
bryceandy <bryceandy@rocketmail.com>
2022-05-14 17:01:04 +0300

    Place a search user component receiving a bank id prop

---
+++ src/components/SearchUser.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="relative max-w-[320px]">
+    <svg id="search-icon" class="w-5 h-5 fill-current absolute transform translate-y-3/4 translate-x-1/2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M30 28.59 22.45 21A11 11 0 1 0 21 22.45L28.59 30ZM5 14a9 9 0 1 1 9 9 9 9 0 0 1-9-9Z" /><path fill="none" d="M0 0h32v32H0z" /></svg>
+    <input
+      class="rounded-full pl-8 pr-6 py-3 border focus:outline-none focus:ring-2 focus:ring-offset-2
+      focus:ring-emerald-200 w-full"
+      placeholder="Search bank users..."
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{ bankId: string }>()
+</script>
--- src/views/BankView.vue
+++ src/views/BankView.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="px-5 sm:px-8 lg:px-20 lg:pb-12">
     <h1 class="text-5xl font-bold">Bank Users</h1>
-    <div class="my-6 flex items-center gap-2">
+    <div class="my-6 flex items-center gap-2 border-b border-emerald-400 pb-4">
       <label for="select-bank">Please select your bank:</label>
       <select
         id="select-bank"
         class="px-4 py-2 rounded-full border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-200"
         v-model="activeBank"
         name="select-bank"
-        >
+      >
         <option :value="id" v-for="{ id, name } in banks" :key="id">{{ name }}</option>
       </select>
     </div>
+    <search-user :bank-id="activeBank" />
   </div>
 </template>
 
 <script setup lang="ts">
 import sourceData from '@/data'
+import SearchUser from '@/components/SearchUser'
 import { ref } from 'vue'
 
 const { banks } = sourceData
