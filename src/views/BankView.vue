9993c645cc04ea1195acc85e3ef121f41f8bcf55
bryceandy <bryceandy@rocketmail.com>
2022-05-14 16:01:57 +0300

    Style select dropdown, remove unused reactivity

--- src/views/BankView.vue
+++ src/views/BankView.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="px-5 sm:px-8 lg:px-20 lg:pb-12">
     <h1 class="text-5xl font-bold">Bank Users</h1>
-    <div class="my-6 flex">
-      <label for="select-bank">Please select a bank:</label>
-      <select v-model="activeBank" name="select-bank" id="select-bank" class="px-4 py-3 rounded-full">
+    <div class="my-6 flex items-center gap-2">
+      <label for="select-bank">Please select your bank:</label>
+      <select
+        id="select-bank"
+        class="px-4 py-2 rounded-full border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-200"
+        v-model="activeBank"
+        name="select-bank"
+        >
         <option :value="id" v-for="{ id, name } in banks" :key="id">{{ name }}</option>
       </select>
     </div>
@@ -12,8 +17,8 @@
 
 <script setup>
 import sourceData from '@/data'
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 
-const banks = reactive(sourceData.banks)
-const activeBank = ref('')
+const { banks } = sourceData
+const activeBank = ref(banks[0].id)
 </script>
