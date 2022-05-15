18e8b1ff1625cf85ef10b91360beb3a44a74b35a
bryceandy <bryceandy@rocketmail.com>
2022-05-15 23:13:37 +0300

    Get rid of redundant reactivity

--- src/composables/useSearchUsers.ts
+++ src/composables/useSearchUsers.ts
@@ -1,8 +1,8 @@
-import { computed, ref, ToRef } from 'vue'
+import { computed, ref } from 'vue'
 
-export default function useSearchUsers (users: ToRef<User[]>) {
+export default function useSearchUsers (users: User[]) {
   const searchQuery = ref<string>('')
-  const usersMatchingSearchQuery = computed<User[] | []>(() => users.value.filter(
+  const usersMatchingSearchQuery = computed<User[] | []>(() => users.filter(
     (userObject: User) => userObject.name.includes(searchQuery.value)
   ))
 
