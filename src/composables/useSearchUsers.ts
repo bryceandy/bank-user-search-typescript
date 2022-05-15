7781a61442a78a986467d0d8bace1aceb4c9f073
bryceandy <bryceandy@rocketmail.com>
2022-05-15 23:50:46 +0300

    Search using lowercases

--- src/composables/useSearchUsers.ts
+++ src/composables/useSearchUsers.ts
@@ -3,7 +3,7 @@ import { computed, ref } from 'vue'
 export default function useSearchUsers (users: User[]) {
   const searchQuery = ref<string>('')
   const usersMatchingSearchQuery = computed<User[] | []>(() => users.filter(
-    (userObject: User) => userObject.name.includes(searchQuery.value)
+    (userObject: User) => userObject.name.toLowerCase().includes(searchQuery.value.toLowerCase())
   ))
 
   return { searchQuery, usersMatchingSearchQuery }
